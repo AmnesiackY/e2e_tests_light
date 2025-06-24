1070160d334d9639dddf4c783922af3caa468e49
Yarek <yaroslav.yarovyi@y-tree.com>
2025-06-25 00:26:32 +0300

    move ExtendWith to BaseTest

--- src/test/java/io/testomat/e2e_tests_light/BaseTest.java
+++ src/test/java/io/testomat/e2e_tests_light/BaseTest.java
@@ -1,13 +1,16 @@
 package io.testomat.e2e_tests_light;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.junit5.TextReportExtension;
 import io.github.cdimascio.dotenv.Dotenv;
 import io.testomat.e2e_tests_light.common.Application;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.commons.logging.Logger;
 import org.junit.platform.commons.logging.LoggerFactory;
 
+@ExtendWith({TextReportExtension.class})
 public class BaseTest {
 
     protected static Dotenv env = Dotenv.load();
@@ -35,6 +38,7 @@ public class BaseTest {
 
     static {
         Configuration.baseUrl = baseUrl;
-        Configuration.headless = false;
+        Configuration.headless = true;
+        Configuration.timeout = 10000;
     }
 }
